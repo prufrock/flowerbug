252e9a75480f25b6a19cfc04f190610d77179188
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-04-03 21:53:24 -0500

    Upload file to S3.

--- app/Console/Commands/S3Upload.php
+++ app/Console/Commands/S3Upload.php
@@ -13,6 +13,16 @@ use App\Utilities\Constants;
 class S3Upload extends Command {
 
   /**
+   * Upload successful.
+   */
+  const EXIT_SUCCESS = 0;
+
+  /**
+   * Upload failed.
+   */
+  const EXIT_FAILURE = 1;
+
+  /**
    * The name and signature of the console command.
    *
    * @var string
@@ -21,8 +31,9 @@ class S3Upload extends Command {
     '{path-to-file : The file to upload.} ' .
     '{bucket : The name of the bucket to upload to.} ' .
     '{path-to-destination : The name of the file to upload to.} ' .
+    '{content-type : The content type of the file.} ' .
     '{--z|gzip : compress the file, upload it and set content encoding.} ' .
-    '{--p|public make the file public.}'.
+    '{--p|public : make the file public.}'.
     '{--d|dry-run : Shows what would have been uploaded.}';
 
   /**
@@ -55,21 +66,36 @@ class S3Upload extends Command {
    */
   public function handle() {
 
-    $aDryRun = false;
-    $pathToFile = '';
-    $bucket = '';
-    $pathToDestination = '';
-    $compress = false;
-    $makePublic = false;
+    $aDryRun = $this->option('dry-run');
+    $pathToFile = $this->argument('path-to-file');
+    $bucket = $this->argument('bucket');
+    $pathToDestination = $this->argument('path-to-destination');
+    $contentType = $this->argument('content-type');
+    $compress = $this->option('gzip');
+    $makePublic = $this->option('public');
+    $verbose = $this->option('verbose');
     $succeeded = false;
 
 
     if (!$aDryRun) {
-      $succeeded = $this->service->upload($pathToFile, $bucket, $pathToDestination, $compress, $makePublic);
+
+      $succeeded = $this->service->upload($pathToFile, $bucket, $pathToDestination, $contentType, $compress, $makePublic);
+    }
+
+    if ($verbose) {
+      if ($succeeded) {
+
+        $this->info($pathToFile . ' ' . Constants::COPY_TO_SIGIL . ' ' . $bucket . ' ' . $pathToDestination);
+      } else {
+
+        $this->info($this->service->getErrorMessage());
+      }
     }
 
     if ($succeeded) {
-      $this->info($pathToFile . ' ' . Constants::COPY_TO_SIGIL . ' ' . $bucket . ' ' . $pathToDestination);
+      return self::EXIT_SUCCESS;
+    } else {
+      return self::EXIT_FAILURE;
     }
   }
 }
--- app/Services/S3UploadService.php
+++ app/Services/S3UploadService.php
@@ -1,5 +1,8 @@
 <?php namespace App\Services;
 
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
+
 /**
  * Class S3UploadService upload a file to S3.
  * @package App\Services\S3UploadService
@@ -7,17 +10,70 @@
 class S3UploadService {
 
   /**
+   * Set if an error occurs.
+   * @var string|null
+   */
+  private $errorMessage = null;
+
+  /**
+   * Set if an error occurs.
+   * @return null|string
+   */
+  public function getErrorMessage() {
+    
+    return $this->errorMessage;
+  }
+
+  /**
    * Uploads a file to S3.
    * @param $pathToFile
    * @param $bucket
    * @param $pathToDestination
+   * @param $contentType
    * @param $compress
    * @param $makePublic
    * @return bool
    */
-  public function upload($pathToFile, $bucket, $pathToDestination, $compress, $makePublic) {
+  public function upload($pathToFile, $bucket, $pathToDestination, $contentType, $compress, $makePublic) {
 
-    print "uploaded" . PHP_EOL;
-    return true;
+    $succeeded = false;
+    $client = null;
+    $args = null;
+    $this->errorMessage = null;
+
+    $client = S3Client::factory();
+
+    $args = [
+      'Bucket' => $bucket,
+      'Key' => $pathToDestination,
+      'SourceFile' => $pathToFile,
+      'ContentType' => $contentType
+    ];
+
+    if ($compress) {
+
+      //TODO compress the file
+      $args['ContentEncoding'] = 'gzip';
+    }
+
+    if ($makePublic) {
+
+      http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObjectAcl
+      $args['ACL'] = 'public-read';
+    } else {
+
+      $args['ACL'] = 'private';
+    }
+
+    try {
+
+      // http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObject
+      $succeeded = $client->putObject($args);
+    } catch (S3Exception $e) {
+
+      $this->errorMessage = $e->getMessage();
+    }
+
+    return $succeeded;
   }
 }
