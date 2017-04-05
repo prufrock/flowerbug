ef7d6ab0e5f635d3b113230dbb88a4e644d80419
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-04-04 21:44:08 -0500

    Update base test classes for 5.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,25 +1,8 @@
-<?php
+<?php namespace Tests;
 
-abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
-{
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+abstract class TestCase extends BaseTestCase {
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
+  use CreatesApplication;
 }
