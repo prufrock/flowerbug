3c3ec8c382959acaf6c3733f36aafad18151389c
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-07-29 15:40:21 -0500

    Test for the PaymentProcessor Role and fix phpunit issue.
    I had to update composer to fix a phpunit issue.

--- tests/Unit/Domain/PaymentProcessorTest.php
+++ tests/Unit/Domain/PaymentProcessorTest.php
@@ -4,7 +4,15 @@ use Tests\TestCase;
 
 class PaymentProcessorTest extends TestCase {
 
+  public function testPaymentProcessorRole() {
+
+    $paymentProcessor = new \App\Domain\PaymentProccesor();
+
+    $this->assertTrue(method_exists($paymentProcessor, 'process'));
+  }
+
   public function testNew() {
+
     $this->assertInstanceOf(
       \App\Domain\PaymentProccesor::class,
       new \App\Domain\PaymentProccesor()
