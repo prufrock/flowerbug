8fe23ad4800c127fa2dc4a360cff57e476619e05
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-08-11 21:54:02 -0500

    WIP: IpnResponder

--- app/Domain/IpnResponder.php
+++ app/Domain/IpnResponder.php
@@ -2,4 +2,30 @@
 
 class IpnResponder {
 
+  private $ipnVars;
+  private $validationHeader;
+  private $validationCmd;
+  private $validationUrl;
+  private $validationPort;
+  private $validationTimeout;
+  private $validationExpectedResponse;
+  private $invalidExpectedResponse;
+  private $ipnDataStore;
+  private $logger;
+
+  public function __construct(\App\Domain\FilePointerProxy $fproxy) {
+
+  }
+
+  public function create($params) {
+
+    foreach($params as $key => $value) {
+      $this->$key = $value;
+    }
+  }
+
+  public function isVerified() {
+
+    return true;
+  }
 }
--- tests/Unit/Domain/IpnResponderTest.php
+++ tests/Unit/Domain/IpnResponderTest.php
@@ -1,16 +1,60 @@
 <?php namespace Tests\Unit\Domain;
 
 use Tests\TestCase;
+use Mockery as m;
 
 class IpnResponderTest extends TestCase {
 
+  private function containerOverride($fullClassName, $override) {
+    $this->app->bind($fullClassName, $override);
+  }
+
   public function testNew() {
 
-    $responder = new \App\Domain\IpnResponder();
+    $this->containerOverride('\App\Domain\FilePointerProxy', m::mock('\App\Domain\FilePointerProxy'));
+    $responder = $this->app->make(\App\Domain\IpnResponder::class);
 
     $this->assertInstanceOf(
       \App\Domain\IpnResponder::class,
       $responder
     );
+  }
+
+  public function testIsVerified() {
+
+    $payment = ['txn_id' => 1];
+    $validationHeader = "";
+    $validationHeader .= "POST /cgi-bin/webscr HTTP/1.0\r\n";
+    $validationHeader .= "Content-Type: "
+      . "application/x-www-form-urlencoded\r\n";
+    $validationHeader .= "Content-Length: <contentlength>\r\n\r\n";
+    $validationCmd = 'cmd=_notify-validate';
+    $validationUrl = 'ssl://www.paypal.com';
+    $validationPort = 443;
+    $validationTimeout = 30;
+    $validationExpectedResponse = "VERIFIED";
+    $invalidExpectedResponse = "INVALID";
+    $ipnDataStore = new \stdClass();
+    $logger = new \stdClass();
+
+    $fproxy = m::mock('\App\Domain\FilePointerProxy');
+    $this->containerOverride('\App\Domain\FilePointerProxy', $fproxy);
+
+    $responder = $this->app->make(\App\Domain\IpnResponder::class);
+
+    $responder->create([
+      'ipnVars' => $payment,
+      'validationHeader' => $validationHeader,
+      'validationCmd' => $validationCmd,
+      'validationUrl' => $validationUrl,
+      'validationPort' => $validationPort,
+      'validationTimeout' => $validationTimeout,
+      'validationExpectedResponse' => $validationExpectedResponse,
+      'invalidExpectedResponse' => $invalidExpectedResponse,
+      'ipnDataStore' => $ipnDataStore,
+      'logger' => $logger
+    ]);
+
+    $this->assertTrue($responder->isVerified());
   }
 }
