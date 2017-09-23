d90b4c3aac3a6f514ff58eb5d033f65de2f4d670
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-09-22 21:26:19 -0500

    WIP: SaleNotifier

---
+++ app/Domain/SaleNotifier.php
@@ -0,0 +1,8 @@
+<?php namespace App\Domain;
+
+class SaleNotifier {
+
+  public function notify() {
+
+  }
+}
---
+++ tests/Unit/Domain/SaleNotifierTest.php
@@ -0,0 +1,19 @@
+<?php namespace Tests\Unit\Domain;
+
+use Tests\TestCase;
+
+class SaleNotifierTest extends TestCase {
+
+  public function testNew() {
+
+    $this->assertInstanceOf(
+      \App\Domain\SaleNotifier::class,
+      new \App\Domain\SaleNotifier()
+    );
+  }
+
+  public function testSuccessfulNotify() {
+
+    $this->assertNull((new \App\Domain\SaleNotifier)->notify());
+  }
+}
