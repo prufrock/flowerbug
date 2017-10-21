353d32fd2c5c14d0d24d5f80fb15306fbf6a2a76
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-10-20 20:07:32 -0500

    Fixed a typo.

--- app/Http/Controllers/IpnController.php
+++ app/Http/Controllers/IpnController.php
@@ -1,12 +1,12 @@
 <?php namespace App\Http\Controllers;
 
-use App\Domain\PaymentProccesor;
+use App\Domain\PaymentProcessor;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class IpnController {
 
-  public function index(Request $request, PaymentProccesor $paymentProcessor) {
+  public function index(Request $request, PaymentProcessor $paymentProcessor) {
 
     $paymentProcessor->process($request->all());
     return response('', Response::HTTP_OK);
