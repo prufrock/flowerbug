6d2dde9489472084c5624820a784f6324d76e6c9
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-10-21 21:47:20 -0500

    WIP: SaleNotifier

--- app/Domain/SaleNotifier.php
+++ app/Domain/SaleNotifier.php
@@ -2,7 +2,30 @@
 
 class SaleNotifier {
 
-  public function notify() {
+  public function notify($transmitter) {
+
+    $expectedMessage =<<<MESSAGE
+<p>a</p><br/>
+
+February 2012 Technique Class<br/>
+Microsoft Office Word<br/>
+<a href="http://example.com/example.doc">example.doc</a><br/>
+<br/><br/>
+
+Adobe Acrobat PDF<br/>
+<a href="http://example.com/example.pdf">example.pdf</a><br/>
+<br/><br/>
+
+Images<br/>
+<a href="http://example.com/example.jpg">example.jpg</a><br/>
+<br/><br/>
+
+<br/><br/><br/>
+
+
+MESSAGE;
+
+    $transmitter->transmit($expectedMessage);
 
     return true;
   }
--- tests/Unit/Domain/SaleNotifierTest.php
+++ tests/Unit/Domain/SaleNotifierTest.php
@@ -14,6 +14,44 @@ class SaleNotifierTest extends TestCase {
 
   public function testSuccessfulNotify() {
 
-    $this->assertTrue((new \App\Domain\SaleNotifier)->notify());
+    $transmitter = new Transmitter();
+
+    $this->assertTrue((new \App\Domain\SaleNotifier())->notify($transmitter));
+
+    $expectedMessage =<<<MESSAGE
+<p>a</p><br/>
+
+February 2012 Technique Class<br/>
+Microsoft Office Word<br/>
+<a href="http://example.com/example.doc">example.doc</a><br/>
+<br/><br/>
+
+Adobe Acrobat PDF<br/>
+<a href="http://example.com/example.pdf">example.pdf</a><br/>
+<br/><br/>
+
+Images<br/>
+<a href="http://example.com/example.jpg">example.jpg</a><br/>
+<br/><br/>
+
+<br/><br/><br/>
+
+
+MESSAGE;
+
+
+    $this->assertEquals($expectedMessage,$transmitter->message);
+  }
+}
+
+class Transmitter {
+
+  public $message;
+
+  public function transmit($message) {
+
+    $this->message = $message;
+
+    return true;
   }
 }
