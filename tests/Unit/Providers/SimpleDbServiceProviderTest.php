17a803e21bc050d2477aa8e366aa38d4c6bb5895
David Kanenwisher <dkanenwisher@myriadmobile.com>
2017-12-29 14:20:20 -0600

    Upgraded to Laravel 5.5.
    It's time.

--- tests/Unit/Providers/SimpleDbServiceProviderTest.php
+++ tests/Unit/Providers/SimpleDbServiceProviderTest.php
@@ -10,5 +10,7 @@ class SimpleDbServiceProviderTest extends TestCase {
   public function testInjection() {
 
     $object = $this->app->make(SimpleDbClient::class);
+
+    $this->assertEquals(SimpleDbClient::class, get_class($object));
   }
 }
