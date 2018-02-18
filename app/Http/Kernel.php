77c2c90c55775eb7f543ef4c38cd667b0442dc52
David Kanenwisher <dkanenwisher@myriadmobile.com>
2018-02-17 22:10:10 -0600

    Upgrade Laravel to 5.6.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -56,10 +56,11 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
-            'bucket' => 'your-bucket',
+            'url' => env('AWS_URL'),
+            'key' => env('AWS_KEY'),
+            'secret' => env('AWS_SECRET'),
+            'region' => env('AWS_REGION'),
+            'bucket' => env('AWS_BUCKET'),
         ],
 
     ],
