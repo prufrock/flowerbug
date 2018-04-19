1bc07671b00907c11be93d9f71ef9403e2952522
David Kanenwisher <dkanenwisher@myriadmobile.com>
2018-04-18 22:07:02 -0500

    Refactoring the tests.

--- tests/Unit/Domain/ProjectTest.php
+++ tests/Unit/Domain/ProjectTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Tests\Unit\Domain;
 
@@ -22,40 +22,36 @@ class ProjectTest extends TestCase
         $this->assertEquals('February 2017 Technique Class', $project->getTitle());
     }
 
-    public function testFind()
+    public function testFindReturnsACollection()
     {
         $simpleDbClient = m::mock(\Aws\SimpleDb\SimpleDbClient::class);
-        $simpleDbClient->shouldReceive('select')->withAnyArgs()->with([
-                'SelectExpression' => 'select * from '.config('flowerbug.simpledb.projects_domain').' where id = \'technique201702\' or id = \'technique201703\' or id = \'technique201704\'',
-                'ConsistentRead' => true,
-            ])->andReturn([
-                'Items' => [
-                    [
-                        'Name' => 'technique201702',
-                        'Attributes' => [
-                            ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
-                        ],
-                    ],
-                    [
-                        'Name' => 'technique201703',
-                        'Attributes' => [
-                            ['Name' => 'name', 'Value' => 'March 2017 Technique Class'],
-                        ],
-                    ],
-                    [
-                        'Name' => 'technique201704',
-                        'Attributes' => [
-                            ['Name' => 'name', 'Value' => 'May 2017 Technique Class'],
-                        ],
-                    ],
-                ],
-            ])->once();
+        $simpleDbClient->shouldReceive('select')->withAnyArgs()->andReturn([
+            'Items' => [],
+        ]);
         $guideGateway = m::mock(\App\Domain\Guide::class);
         $project = new \App\Domain\Project($simpleDbClient, $guideGateway);
-        $found = $project->find(['technique201702', 'technique201703', 'technique201704']);
+        $found = $project->find(['technique201702']);
 
         $this->assertInstanceOf(\Illuminate\Support\Collection::class, $found);
-        $this->assertEquals(3, $found->count());
+    }
+
+    public function testFindReturnsACollectionOfProjects()
+    {
+        $simpleDbClient = m::mock(\Aws\SimpleDb\SimpleDbClient::class);
+        $simpleDbClient->shouldReceive('select')->withAnyArgs()->andReturn([
+            'Items' => [
+                [
+                    'Name' => 'technique201702',
+                    'Attributes' => [
+                        ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
+                    ],
+                ],
+            ],
+        ]);
+        $guideGateway = m::mock(\App\Domain\Guide::class);
+        $project = new \App\Domain\Project($simpleDbClient, $guideGateway);
+        $found = $project->find(['technique201702']);
+
         $this->assertInstanceOf(\App\Domain\Project::class, $found->first());
     }
 
@@ -64,19 +60,16 @@ class ProjectTest extends TestCase
         $guideGateway = m::mock(\App\Domain\Guide::class);
         $guideGateway->shouldReceive('find')->with('technique201702')->andReturn(collect([$guideGateway]));
         $simpleDbClient = m::mock(\Aws\SimpleDb\SimpleDbClient::class);
-        $simpleDbClient->shouldReceive('select')->withAnyArgs()->with([
-                'SelectExpression' => 'select * from '.config('flowerbug.simpledb.projects_domain').' where id = \'technique201702\'',
-                'ConsistentRead' => true,
-            ])->andReturn([
-                'Items' => [
-                    [
-                        'Name' => 'technique201702',
-                        'Attributes' => [
-                            ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
-                        ],
+        $simpleDbClient->shouldReceive('select')->withAnyArgs()->andReturn([
+            'Items' => [
+                [
+                    'Name' => 'technique201702',
+                    'Attributes' => [
+                        ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
                     ],
                 ],
-            ])->once();
+            ],
+        ]);
         $project = (new \App\Domain\Project($simpleDbClient, $guideGateway))->find('technique201702');
         $guides = $project->first()->getGuides();
 
@@ -91,19 +84,16 @@ class ProjectTest extends TestCase
         $guideGateway->shouldReceive('find')->with('technique201702')->andReturn(collect([$guideGateway]));
         $guideGateway->shouldReceive('getFileType')->andReturn('pdf')->once();
         $simpleDbClient = m::mock(\Aws\SimpleDb\SimpleDbClient::class);
-        $simpleDbClient->shouldReceive('select')->withAnyArgs()->with([
-                'SelectExpression' => 'select * from '.config('flowerbug.simpledb.projects_domain').' where id = \'technique201702\'',
-                'ConsistentRead' => true,
-            ])->andReturn([
-                'Items' => [
-                    [
-                        'Name' => 'technique201702',
-                        'Attributes' => [
-                            ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
-                        ],
+        $simpleDbClient->shouldReceive('select')->withAnyArgs()->andReturn([
+            'Items' => [
+                [
+                    'Name' => 'technique201702',
+                    'Attributes' => [
+                        ['Name' => 'name', 'Value' => 'February 2017 Technique Class'],
                     ],
                 ],
-            ])->once();
+            ],
+        ]);
         $project = (new \App\Domain\Project($simpleDbClient, $guideGateway))->find('technique201702');
         $guides = $project->first()->getGuides('pdf');
 
@@ -111,14 +101,14 @@ class ProjectTest extends TestCase
         $this->assertEquals(1, $guides->count());
         $this->assertInstanceOf(\App\Domain\Guide::class, $guides->first());
     }
-    
-    public function testGetAllProjectsReturnsACollection() 
+
+    public function testGetAllProjectsReturnsACollection()
     {
         $project = resolve(\App\Domain\Project::class);
         $this->assertInstanceOf(\Illuminate\Support\Collection::class, $project->all());
     }
-    
-    public function testGetAllProjectsReturnsACollectionWithProjectsInIt() 
+
+    public function testGetAllProjectsReturnsACollectionWithProjectsInIt()
     {
         $project = resolve(\App\Domain\Project::class);
         $this->assertInstanceOf(\App\Domain\Project::class, $project->all()->first());
